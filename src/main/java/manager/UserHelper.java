c1013bdf05676a8e47b0ee5cfdf40caf77b5e629
Alisochka1991 <alisiaagranov@gmail.com>
2021-10-26 12:06:41 +0300

    comm4

--- src/main/java/manager/UserHelper.java
+++ src/main/java/manager/UserHelper.java
@@ -1,4 +1,9 @@
 package manager;
 
-public class UserHelper {
+import org.openqa.selenium.WebDriver;
+
+public class UserHelper extends HelperBase {
+    public UserHelper(WebDriver wd) {
+        super(wd);
+    }
 }
