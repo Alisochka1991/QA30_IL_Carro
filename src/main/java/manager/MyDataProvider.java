7a5db3fd349ff5e62f5425e8d63ba34191c7d214
Alisochka1991 <alisiaagranov@gmail.com>
2021-12-06 22:10:34 +0200

    comm

--- src/main/java/manager/MyDataProvider.java
+++ src/main/java/manager/MyDataProvider.java
@@ -3,6 +3,10 @@ package manager;
 import models.User;
 import org.testng.annotations.DataProvider;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -28,6 +32,25 @@ public class MyDataProvider {
         List<Object[]> list = new ArrayList<>();
         list.add(new Object[]{new User().withEmail("alisiaagranov@gmail.com").withPassword("212229Alisa")});
         list.add(new Object[]{new User().withEmail("alisiaagranov@gmail.com").withPassword("212229Alisa")});
+        return list.iterator();
+    }
+    @DataProvider
+    public Iterator<Object[]>registrationCSV() throws IOException {
+        List<Object[]>list = new ArrayList<>();
+
+        BufferedReader reader= new BufferedReader(new FileReader("src/test/resources/data.csv"));
+        String line = reader.readLine();
+        while (line!=null)
+        {
+            String[] split = line.split(",");
+            list.add(new Object[]{new User()
+                    .withName(split[0])
+                    .withLastname(split[1])
+                    .withEmail(split[2])
+                    .withPassword(split[3])});
+            line= reader.readLine();
+        }
+
         return list.iterator();
     }
 }
